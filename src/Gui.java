d25e8256d5352370a4c8aa0016469c45ad09f527
Christopher Hosken <hoskenchristopher@gmail.com>
2021-05-07 21:42:12 +0700

    basic ui implemented

--- src/Gui.java
+++ src/Gui.java
@@ -1,13 +1,24 @@
 import javafx.application.Application;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 public class Gui extends Application {
-    @Override
-    public void start(Stage stage) {
-        stage.show();
-    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("AP Physics Engine");
+        stage.getIcons().add(new Image("assets\\atom.png"));
+
+        StackPane root = new StackPane();
+        
+        stage.setScene(new Scene(root, 1728, 972));
+        stage.show();
+    }
+
 }
