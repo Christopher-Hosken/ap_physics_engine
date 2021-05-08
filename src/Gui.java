85e59a1ab5a4e4ab0b6da202d469a0e6a22ba0a6
Christopher Hosken <hoskenchristopher@gmail.com>
2021-05-08 14:02:52 +0700

    icons implemented

--- src/Gui.java
+++ src/Gui.java
@@ -1,20 +1,19 @@
 import javax.swing.SwingUtilities;
 
-import com.jogamp.nativewindow.util.Dimension;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLJPanel;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
-import javafx.geometry.Dimension2D;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,7 +30,7 @@ public class Gui extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("AP Physics Engine");
-        stage.getIcons().add(new Image("assets\\images\\atom.png"));
+        stage.getIcons().add(new Image("assets\\images\\icons\\atom.png"));
 
         StackPane root = new StackPane();
         root.setId("ROOTNODE");
@@ -40,6 +39,23 @@ public class Gui extends Application {
         rect.setArcWidth(25);
         root.setClip(rect);
 
+        final GLProfile profile = GLProfile.getDefault();
+        final GLCapabilities capabilities = new GLCapabilities(profile);
+        
+        GLJPanel canvas = new GLJPanel(capabilities);
+
+        SwingNode swingNode = new SwingNode();
+
+        root.getChildren().add(swingNode);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                swingNode.setContent(canvas);
+            }
+        });
+
+
+        //#region header
         StackPane header = new StackPane();
         header.setTranslateY(-436);
         header.setMaxHeight(100);
@@ -53,9 +69,12 @@ public class Gui extends Application {
         hLine.setStroke(lineColor);
         header.getChildren().add(hLine);
 
-        ImageView logo = new ImageView(new Image("assets/images/atom.png"));
-        logo.setFitWidth(69);
-        logo.setFitHeight(69);
+        Button logo = new Button();
+        logo.getStyleClass().add("icon-button");
+        logo.setId("LOGO");
+        logo.getStyleClass().add("icon-button");
+        logo.setMaxHeight(70);
+        logo.setMaxWidth(70);
         logo.setTranslateX(-800);
         header.getChildren().add(logo);
 
@@ -71,12 +90,52 @@ public class Gui extends Application {
         vLine1.setStroke(lineColor);
         header.getChildren().add(vLine1);
 
+        Button toggleMenu = new Button();
+        toggleMenu.getStyleClass().add("icon-button");
+        toggleMenu.setId("TOGMENU");
+        toggleMenu.setMaxHeight(70);
+        toggleMenu.setMaxWidth(70);
+        toggleMenu.setTranslateX(-290);
+        header.getChildren().add(toggleMenu);
+
         Line vLine2 = new Line();
         vLine2.setStartY(0);
         vLine2.setEndY(100);
         vLine2.setTranslateX(-230);
         vLine2.setStroke(lineColor);
         header.getChildren().add(vLine2);
+
+        Button addSphere = new Button();
+        addSphere.getStyleClass().add("icon-button");
+        addSphere.setId("ADDSPH");
+        addSphere.setMaxHeight(70);
+        addSphere.setMaxWidth(70);
+        addSphere.setTranslateX(-170);
+        header.getChildren().add(addSphere);
+
+        Button addCube = new Button();
+        addCube.getStyleClass().add("icon-button");
+        addCube.setId("ADDCUBE");
+        addCube.setMaxHeight(70);
+        addCube.setMaxWidth(70);
+        addCube.setTranslateX(-40);
+        header.getChildren().add(addCube);
+
+        Button importModel = new Button();
+        importModel.getStyleClass().add("icon-button");
+        importModel.setId("IMPORTMOD");
+        importModel.setMaxHeight(70);
+        importModel.setMaxWidth(70);
+        importModel.setTranslateX(90);
+        header.getChildren().add(importModel);
+
+        Button addLight = new Button();
+        addLight.getStyleClass().add("icon-button");
+        addLight.setId("ADDLIGHT");
+        addLight.setMaxHeight(70);
+        addLight.setMaxWidth(70);
+        addLight.setTranslateX(210);
+        header.getChildren().add(addLight);
 
         Line vLine3 = new Line();
         vLine3.setStartY(0);
@@ -85,6 +144,22 @@ public class Gui extends Application {
         vLine3.setStroke(lineColor);
         header.getChildren().add(vLine3);
 
+        Button toggleWire = new Button();
+        toggleWire.getStyleClass().add("icon-button");
+        toggleWire.setId("TOGWIRE");
+        toggleWire.setMaxHeight(70);
+        toggleWire.setMaxWidth(70);
+        toggleWire.setTranslateX(330);
+        header.getChildren().add(toggleWire);
+
+        Button toggleVisibility = new Button();
+        toggleVisibility.getStyleClass().add("icon-button");
+        toggleVisibility.setId("TOGVIS");
+        toggleVisibility.setMaxHeight(70);
+        toggleVisibility.setMaxWidth(70);
+        toggleVisibility.setTranslateX(440);
+        header.getChildren().add(toggleVisibility);
+
         Line vLine4 = new Line();
         vLine4.setStartY(0);
         vLine4.setEndY(100);
@@ -92,23 +167,48 @@ public class Gui extends Application {
         vLine4.setStroke(lineColor);
         header.getChildren().add(vLine4);
 
+        Button toggleTheme = new Button();
+        toggleTheme.getStyleClass().add("icon-button");
+        toggleTheme.setId("TOGTHEME");
+        toggleTheme.setMaxHeight(70);
+        toggleTheme.setMaxWidth(70);
+        toggleTheme.setTranslateX(560);
+        header.getChildren().add(toggleTheme);
 
-/*
-        final GLProfile profile = GLProfile.getDefault();
-        final GLCapabilities capabilities = new GLCapabilities(profile);
-        
-        GLJPanel canvas = new GLJPanel(capabilities);
+        Button info = new Button();
+        info.getStyleClass().add("icon-button");
+        info.setId("INFO");
+        info.setMaxHeight(70);
+        info.setMaxWidth(70);
+        info.setTranslateX(645);
+        header.getChildren().add(info);
 
-        SwingNode swingNode = new SwingNode();
+        Button bug = new Button();
+        bug.getStyleClass().add("icon-button");
+        bug.setId("BUG");
+        bug.setMaxHeight(70);
+        bug.setMaxWidth(70);
+        bug.setTranslateX(725);
+        header.getChildren().add(bug);
 
-        root.getChildren().add(swingNode);
+        Button close = new Button();
+        close.getStyleClass().add("icon-button");
+        close.setId("CLOSE");
+        close.setMaxHeight(70);
+        close.setMaxWidth(70);
+        close.setTranslateX(810);
+        header.getChildren().add(close);
+        //#endregion
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                swingNode.setContent(canvas);
-            }
-        });
-*/
+        //#region menu
+        StackPane menu = new StackPane();
+        menu.setId("MENU");
+        menu.setMaxSize(365, 872);
+        menu.setTranslateX(682);
+        menu.setTranslateY(51);
+        root.getChildren().add(menu);
+
+        //#endregion
 
         Scene scene = new Scene(root, 1728, 972);
         scene.setFill(Color.TRANSPARENT);
@@ -116,5 +216,13 @@ public class Gui extends Application {
         stage.setScene(scene);
         stage.getScene().getStylesheets().setAll("main.css");
         stage.show();
+    }
+
+    private static SVGPath createPath(String d, String fill, String hoverFill) {
+        SVGPath path = new SVGPath();
+        path.getStyleClass().add("svg");
+        path.setContent(d);
+        path.setStyle("-fill:" + fill + ";-hover-fill:"+hoverFill+';');
+        return path;
     }
 }
