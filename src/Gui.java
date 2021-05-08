a4989f7868a8608e9ea15f7886d15b78485a8904
Christopher Hosken <hoskenchristopher@gmail.com>
2021-05-08 11:20:18 +0700

    ui updates

--- src/Gui.java
+++ src/Gui.java
@@ -1,22 +1,28 @@
 import javax.swing.SwingUtilities;
 
+import com.jogamp.nativewindow.util.Dimension;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLJPanel;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
+import javafx.geometry.Dimension2D;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Gui extends Application {
+    Color lineColor = new Color(0.4, 0.4, 0.4, 1);
 
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -30,44 +36,64 @@ public class Gui extends Application {
         StackPane root = new StackPane();
         root.setId("ROOTNODE");
         Rectangle rect = new Rectangle(1728, 972);
-        rect.setArcHeight(35);
-        rect.setArcWidth(35);
+        rect.setArcHeight(25);
+        rect.setArcWidth(25);
         root.setClip(rect);
 
         StackPane header = new StackPane();
-        header.setTranslateY(-426);
-        header.setMaxHeight(120);
+        header.setTranslateY(-436);
+        header.setMaxHeight(100);
         header.setId("HEADER");
         root.getChildren().add(header);
 
-        ImageView icon = new ImageView(new Image("assets/images/atom.png"));
-        icon.setFitHeight(90);
-        icon.setFitWidth(90);
-        icon.setTranslateX(-780);
-        header.getChildren().add(icon);
-
-        Button min = new Button();
-        min.setMaxSize(60, 60);
-        min.setTranslateX(767);
-        min.setTranslateY(-30);
-        min.setGraphic(new ImageView(new Image("assets/images/min.png")));
-        header.getChildren().add(min);
-
-        Button close = new Button();
-        close.setMaxSize(60, 60);
-        close.setTranslateX(832);
-        close.setTranslateY(-30);
-        close.setGraphic(new ImageView(new Image("assets/images/close.png")));
-        close.setId("CLOSE");
-        header.getChildren().add(close);
-
-        Rectangle hLine = new Rectangle(0, 0, 168, 2);
-        hLine.setTranslateX(780);
-        hLine.setTranslateY(2);
-        hLine.setFill(Color.WHITE);
+        Line hLine = new Line();
+        hLine.setStartX(-900);
+        hLine.setEndX(900);
+        hLine.setTranslateY(50);
+        hLine.setStroke(lineColor);
         header.getChildren().add(hLine);
 
-        /*
+        ImageView logo = new ImageView(new Image("assets/images/atom.png"));
+        logo.setFitWidth(69);
+        logo.setFitHeight(69);
+        logo.setTranslateX(-800);
+        header.getChildren().add(logo);
+
+        Label title = new Label("AP Physics Engine");
+        title.setTextFill(Color.WHITE);
+        title.setTranslateX(-590);
+        header.getChildren().add(title);
+
+        Line vLine1 = new Line();
+        vLine1.setStartY(0);
+        vLine1.setEndY(100);
+        vLine1.setTranslateX(-350);
+        vLine1.setStroke(lineColor);
+        header.getChildren().add(vLine1);
+
+        Line vLine2 = new Line();
+        vLine2.setStartY(0);
+        vLine2.setEndY(100);
+        vLine2.setTranslateX(-230);
+        vLine2.setStroke(lineColor);
+        header.getChildren().add(vLine2);
+
+        Line vLine3 = new Line();
+        vLine3.setStartY(0);
+        vLine3.setEndY(100);
+        vLine3.setTranslateX(270);
+        vLine3.setStroke(lineColor);
+        header.getChildren().add(vLine3);
+
+        Line vLine4 = new Line();
+        vLine4.setStartY(0);
+        vLine4.setEndY(100);
+        vLine4.setTranslateX(500);
+        vLine4.setStroke(lineColor);
+        header.getChildren().add(vLine4);
+
+
+/*
         final GLProfile profile = GLProfile.getDefault();
         final GLCapabilities capabilities = new GLCapabilities(profile);
         
@@ -82,7 +108,7 @@ public class Gui extends Application {
                 swingNode.setContent(canvas);
             }
         });
-        */
+*/
 
         Scene scene = new Scene(root, 1728, 972);
         scene.setFill(Color.TRANSPARENT);
